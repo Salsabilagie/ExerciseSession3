ddf9a855bfa702dfe8123d85c4443b959213968f
Yunita Salsabila <132975753+Salsabilagie@users.noreply.github.com>
2023-05-12 12:07:11 +0700

    Add files via upload

---
+++ nomor2.py
@@ -0,0 +1,4 @@
+if __name__== '__main__':
+    number = int(input("Silahkan masukkan angka berapapun: "))
+    answer = number**2
+    print(f"kuadrat dari{number}:{answer}")
---
+++ nomor6.py
@@ -0,0 +1,14 @@
+if __name__== '__main__':
+    x = int(input('Masukan Jumlah Apel'))
+    y = int(input('Masukan Jumlah Jeruk'))
+    z = int(input('Masukan Jumlah Anggur'))
+    Apel = 10000
+    Jeruk = 15000
+    Anggur = 20000
+    Totalapel = x*Apel
+    Totaljeruk = y*Jeruk
+    Totalanggur = z*Anggur
+    Total = (Totalapel + Totaljeruk + Totalanggur)
+print(f'Total = {Totalapel + Totaljeruk + Totalanggur}')
+
+  
